ab28429a824e5857a266774d71f261e656ecc80f
Oleg <78308457+Eposlol@users.noreply.github.com>
2022-11-07 10:14:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
- const [query, setQuery] = useState({ q: "London"})
+ const [query, setQuery] = useState({ q: "Moscow"})
  const [units, setUnits] = useState('metric')
  const [weather, setWeather] = useState(null)
 
